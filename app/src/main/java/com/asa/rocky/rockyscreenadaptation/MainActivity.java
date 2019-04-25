effdb6a7e34659a08a45566f731378892755d6b6
rocky123shine <798795772@qq.com>
2019-04-25 10:26:06 +0800

    Initial commit

---
+++ app/src/main/java/com/asa/rocky/rockyscreenadaptation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.asa.rocky.rockyscreenadaptation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
